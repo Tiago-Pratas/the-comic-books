371dc4d18ebabda56746b9dc4ce15cb21366910e
Tiago-Pratas <prattiago@gmail.com>
2021-06-04 13:58:42 +0200

    FEAT: config register

--- nodejs/app.js
+++ nodejs/app.js
@@ -26,6 +26,15 @@ connect();
 const port = process.env.PORT;
 const app = express();
 
+//allow CORS
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 //use cookieparser
 app.use(
     session({
