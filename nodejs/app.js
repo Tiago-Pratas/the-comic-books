93a4d0bc0ffe12bb50072759dc1c068e2ea038e9
Tiago-Pratas <prattiago@gmail.com>
2021-07-04 10:20:58 +0200

    FIX: fixed Set-cookie on nodejs

--- nodejs/app.js
+++ nodejs/app.js
@@ -46,7 +46,6 @@ app.use(
         cookie: {
             maxAge: 1000 * 60 * 60 * 40,
             path: '/',
-            domain: process.env.CLIENT_URL,
             httpOnly: true,
             sameSite: 'none',
             secure: true,
