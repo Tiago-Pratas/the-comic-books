cb647e2cded1f14226f08268f188cfed3e0c8621
Tiago-Pratas <prattiago@gmail.com>
2021-07-06 18:46:43 +0200

    FIX: fixed cookies on safari in local

--- nodejs/app.js
+++ nodejs/app.js
@@ -35,7 +35,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
     res.header('Access-Control-Allow-Methods', 'GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
 });
 console.log(process.env.NODE_ENV);
